05a94bb71fb381bcbdfce6b5779b56143a09acd6
grinchikk1 <demonchikk1@gmail.com>
2024-02-22 20:02:43 +0200

    add config file

---
+++ client/prettier.config.js
@@ -0,0 +1,7 @@
+export const plugins = ["prettier-plugin-tailwindcss"];
+export const config = {
+  trailingComma: "es5",
+  tabWidth: 4,
+  semi: true,
+  singleQuote: false,
+};
---
+++ client/src/pages/Home/index.tsx
@@ -0,0 +1,18 @@
+function HomePage() {
+  return (
+    <div className="h-dvh flex items-center justify-center">
+      <div className="shrink-0">
+        <img
+          className="h-12 w-12"
+          src="src/assets/react.svg"
+          alt="React Logo"
+        />
+      </div>
+      <div className="px-4">
+        <div className="text-xl font-medium text-black">Market</div>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
